18430f03f75863db700c8f3364499003f036c0aa
Aiden Letourneau <aidenletourneau@gmail.com>
2024-07-12 18:19:49 -0400

    added construction for about page

--- src/routes/about-page.jsx
+++ src/routes/about-page.jsx
@@ -3,7 +3,9 @@ export default function AboutPage() {
 
 
   return (
-    <h1 style='margin-top: 300px; 
-               color: white;'>Under Construction</h1>
+    <div className='page-content'>
+      <h1 className='construction'>Under Construction</h1>
+    </div>
+    
   )
 }
--- src/routes/resume-page.jsx
+++ src/routes/resume-page.jsx
@@ -4,7 +4,9 @@ export default function ResumePage() {
     <>
       <div className='info-region'>
         <embed src="Letourneau_Aiden_Resume.pdf" width="600" height="700"></embed>
+        
       </div>
+      <a className="download-link" href="Letourneau_Aiden_Resume.pdf" download>Download</a>
     </>
 
   )

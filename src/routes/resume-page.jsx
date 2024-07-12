bcc2a6ca3f2fe1b8f046943382e0ec191665965a
Aiden Letourneau <aidenletourneau@gmail.com>
2024-07-12 01:42:40 -0400

    added header to resume page

--- src/routes/resume-page.jsx
+++ src/routes/resume-page.jsx
@@ -1,11 +1,15 @@
-
+import Header from '../Header'
 export default function Resume() {
 
   return (
-
-    <div class="page-content">
-      <embed src="Letourneau_Aiden_Resume.pdf" width="600" height="700"></embed>
-    </div>
+    <>
+      <Header/>
+      <div className="page-content">
+        <div className='info-region'>
+          <embed src="Letourneau_Aiden_Resume.pdf" width="600" height="700"></embed>
+        </div>
+      </div>
+    </>
 
   )
 }
